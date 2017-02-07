fe3ec8f904e0d1beecb0a982d7d6a140314e2b0f
guilhermebaldini@hotmail.com <VOC6w6r7u>
2017-02-07 02:23:10 -0200

    Initial Project

---
+++ Assets/Scripts/BattleController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleController : MonoBehaviour
+{
+
+    private Player pPlayer;
+    private Enemy pEnemy;
+    // Use this for initialization
+    void Start()
+    {
+        pPlayer = FindObjectOfType<Player>();
+        pEnemy = FindObjectOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
